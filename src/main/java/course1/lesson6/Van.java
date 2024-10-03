a7768f32bbc1e06239dd625d46ad5410405fa803
Correctnesss <dunamit@mail.ri>
2024-10-03 14:22:47 +0300

    Lesson 5

---
+++ src/main/java/course1/lesson6/Van.java
@@ -0,0 +1,23 @@
+package course1.lesson6;
+
+public class Van extends Car{
+    String model;
+    int year;
+    int seats;
+
+    public Van(String model, int year, int seats) {
+
+        super(model,year);
+        System.out.println("this is a van constructor");
+        this.seats = seats;
+    }
+
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+}
