eb136f4b8b616acf151f4b0327c334dc4b7d469a
Correctnesss <dunamit@mail.ri>
2025-02-04 16:29:18 +0300

    Lesson 7

---
+++ src/main/java/course2/lesson7/server/AuthService.java
@@ -0,0 +1,25 @@
+package course2.lesson7.server;
+
+/**
+ * Сервис аутентификации
+ */
+public interface AuthService {
+    /**
+     * Запустить сервис.
+     */
+    void start();
+
+    /**
+     * Отключить сервис.
+     */
+    void stop();
+
+    /**
+     * Получить никнейм по логину/паролю
+     *
+     * @param login
+     * @param pass
+     * @return никнейм если найден или null, если такого нет
+     */
+    String getNickByLoginAndPass(String login, String pass);
+}
---
+++ src/main/java/course2/lesson7/server/MyServer.java
@@ -0,0 +1,68 @@
+package course2.lesson7.server;
+
+import course2.lesson7.constans.Constans;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Логика сервера.
+ */
+
+public class MyServer {
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    /**
+     * Сервис аутентификации.
+     */
+    private AuthService authService;
+    /**
+     * Активные клиенты.
+     */
+
+    private List<ClientHandler> clients;
+
+    public MyServer() {
+        try (ServerSocket server = new ServerSocket(Constans.SERVER_PORT)) {
+            authService = new BaseAuthService();//
+            authService.start();
+
+            clients = new ArrayList<>();
+
+            while (true) {
+                System.out.println("Сервер ожидает подключения...");
+                Socket socket = server.accept();
+                System.out.println("Клиент подключился!");
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException ex) {
+            System.out.println("Ошибка в работе сервера.");
+            ex.printStackTrace();
+        } finally {
+            if (authService != null) {
+                authService.stop();
+            }
+        }
+    }
+
+    public synchronized void broadcastMessage(String message) {
+
+        clients.forEach(client -> client.sendMessage(message));
+//        for (ClientHandler client : clients){
+//            client.sendMessage(message);
+//        }
+    }
+
+    public synchronized void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    public synchronized void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+}
